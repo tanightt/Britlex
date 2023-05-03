3057115ca2fab65e4dd33f340b452baf74859f7f
Tania Petruk <127411602+tanightt@users.noreply.github.com>
2023-05-03 22:43:07 +0300

    js

---
+++ index.js
@@ -0,0 +1,16 @@
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//   let message;
+// const totalPrice = pricePerDroid * orderedQuantity;
+//   if (totalPrice > customerCredits) {
+//     message = "Insufficient funds!";
+//   } else {
+//     message = `You ordered ${orderedQuantity} droids, you have ${customerCredits - totalPrice} credits left`;
+//   }
+//   return message;
+// }
+
+// console.log(makeTransaction(3000, 5, 23000));
+// console.log(makeTransaction(1000, 3, 15000));
+// console.log(makeTransaction(500, 10, 5000));
+// console.log(makeTransaction(5000, 10, 8000));
+// console.log(makeTransaction(2000, 8, 10000));
